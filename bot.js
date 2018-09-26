bf9de7215bf4328b416d9580527c8e82ecc0a156
efeaydemir5 <43586787+efeaydemir5@users.noreply.github.com>
2018-09-26 17:16:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(process.env.BOT_TOKEN);
 
 client.on('message', msg => {
    if (msg.content.startsWith(prefix + "yak"))  {
