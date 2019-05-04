acca32b06435f0b27cd70032bdbd336e738c629c
efeaydemir5 <43586787+efeaydemir5@users.noreply.github.com>
2019-05-04 19:29:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -213,7 +213,17 @@ message.channel.send("https://www.twitch.tv/kaanflix");
 
 client.on('message', message => {
 if (message.content === 'z!mokali') {
-message.channel.send("https://www.youtube.com/watch?v=AqNxa-Wp9Tg"
+message.channel.send("https://www.youtube.com/watch?v=AqNxa-Wp9Tg");
 }
 });
 	
+client.on('message', message => {
+if (message.content.toLowerCase() === prefix + "kaanflixkimdir") {
+    var sans = ["Ankara'da yaşamaktadır.", "Denize hasretdir.", "1996 doğumludur.", "Mokali dinlemeyi çok sever.", "Twitch yayını açmayı çok sever.", "Her gün Richardo Milos izler.", "Kölesini kırbaçlamayı çok sever."];
+    var sonuc = sans[Math.floor((Math.random() * sans.length))];
+    const embed = new Discord.RichEmbed()
+    .addField(`***KaanFlix***`, `${sonuc}`)
+    return message.channel.sendEmbed(embed);
+}
+});
+
