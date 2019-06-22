91cad322d043babd1b664e99353e290c42be30be
efeaydemir5 <43586787+efeaydemir5@users.noreply.github.com>
2019-06-22 20:02:51 +0300

    Add files via upload

--- komutlar/kick.js
+++ komutlar/kick.js
@@ -1,14 +1,15 @@
 const { Command } = require('discord.js-commando');
 const Discord = require('discord.js');
 
-module.exports = class ModerationBanCommand extends Command {
+module.exports = class ModerationKickCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'ban',
-			aliases: ['yasakla', 'sunucudan yasakla', 'banla', 'banhammer'],
+			name: 'kick',
+			aliases: ['at', 'sunucudan at', 'kickle', 'kickhammer'],
 			group: 'moderasyon',
-			memberName: 'ban',
-			description: 'İstediğiniz kişiyi sunucudan yasaklar.',
+			memberName: 'kick',
+			description: 'İstediğiniz kişiyi sunucudan atar.',
+			details: ``,
 			guildOnly: true,
 			throttling: {
 				usages: 2,
@@ -19,13 +20,13 @@ module.exports = class ModerationBanCommand extends Command {
 				{
 					key: 'member',
 					label: 'kullanıcı',
-					prompt: 'Kimi sunucudan yasaklamak istersin?',
+					prompt: 'Kimi sunucudan atmak istersin?',
 					type: 'member'
 				},
 				{
 					key: 'sebep',
 					label: 'sebep',
-					prompt: 'Neden bu kişiyi sunucudan yasaklamak istiyorsun?',
+					prompt: 'Neden bu kişiyi sunucudan atmak istiyorsun?',
 					type: 'string'
 				}
 			]
@@ -34,11 +35,11 @@ module.exports = class ModerationBanCommand extends Command {
 
 	hasPermission(msg) {
         if(!msg.guild) return this.client.isOwner(msg.author);
-        return this.client.isOwner(msg.author) || msg.member.hasPermission("BAN_MEMBERS");
+        return this.client.isOwner(msg.author) || msg.member.hasPermission("KICK_MEMBERS");
     }
 
 	async run(msg, args) {
-		let guild = msg.guild;
+		let guild = msg.guild
 		const member = args.member;
 		const user = member.user;
 		const reason = args.sebep;
@@ -51,19 +52,19 @@ module.exports = class ModerationBanCommand extends Command {
 		if (db ==! "evet") return msg.channel.send(`${this.client.emojis.get('464406477851983883')} Lütfen \`mod-log-ayarla\` komutu ile mod-log kanalı belirleyiniz.`);
 		let modlog = vt;
 		if (!modlog) return msg.channel.send(`${this.client.emojis.get('464406477851983883')} Mod-log olarak belirlediğiniz kanal silinmiş, lütfen yeni  bir mod-log kanalı açıp \`mod-log-ayarla\` komutu ile mod-log olarak ayarlayınız.`);
-
-		if (!msg.guild.member(user).bannable) return msg.channel.send(`${this.client.emojis.get('464406477851983883')} Bu kişiyi sunucudan yasaklayamıyorum çünkü \`benden daha yüksek bir role sahip\` ya da \`bana gerekli yetkileri vermedin\`.`);
-		if (user.id === msg.author.id) return msg.say(`${this.client.emojis.get('464406477851983883')} Kendini banlayamazsın.`)
+		if (user.id === msg.author.id) return msg.say(`${this.client.emojis.get('464406477851983883')} Kendini atamazsın.`)
 		if (member.highestRole.calculatedPosition > msg.member.highestRole.calculatedPosition - 1) {
 			return msg.say(`${this.client.emojis.get('464406477851983883')} Bu kişinin senin rollerinden/rolünden daha yüksek rolleri/rolü var.`);
 		}
-		member.send('**' + msg.guild.name + '** sunucusunda `' + msg.author.tag + '` adlı kişi/yetkili tarafından ___' + reason + '___ sebebi ile yasaklandın.')
-		msg.guild.ban(user, 2);
+
+		if (!msg.guild.member(user).kickable) return msg.channel.send(`${this.client.emojis.get('464406477851983883')} Bu kişiyi sunucudan atamıyorum çünkü \`benden daha yüksek bir role sahip\` ya da \`bana gerekli yetkileri vermedin\`.`);
+
+		msg.guild.member(user).kick();
 
 		const embed = new Discord.RichEmbed()
 		.setColor("RANDOM")
 		.setAuthor(`${msg.author.tag} (${msg.author.id})`)
-		.addField(`❯ Eylem:`, `Sunucudan yasaklama`)
+		.addField(`❯ Eylem:`, `Sunucudan atma`)
 		.addField(`❯ Kullanıcı:`, `${user.tag} (${user.id})`)
 		.addField(`❯ Yetkili:`, `${msg.author.tag} (${msg.author.id})`)
 		.addField(`❯ Sebep`, reason)
