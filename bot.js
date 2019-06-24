ed8ce73385acb1256aa740aaf4395b5e6a3cdae8
efeaydemir5 <43586787+efeaydemir5@users.noreply.github.com>
2019-06-24 20:43:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ client.elevation = message => {
   if(!message.guild) {
     return; }
   let permlvl = 0;
-  if (message.member.hasPermission("BANMEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
